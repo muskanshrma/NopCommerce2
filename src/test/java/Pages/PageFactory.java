799a84fe65a74cded93b27d9faddad36cb7ea762
muskanshrma <muskan.sharma@infostride.com>
2022-09-12 02:50:53 -0700

    TestCase 01

--- src/test/java/Pages/PageFactory.java
+++ src/test/java/Pages/PageFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class PageFactory {
     WebDriver driver;
     private LoginPage loginPage;
+    private DiscountsPage discountsPage;
 
     public PageFactory(WebDriver driver) {
         this.driver = driver;
@@ -15,5 +16,11 @@ public class PageFactory {
             loginPage = new LoginPage(driver);
         }
         return loginPage;
+    }
+    public DiscountsPage getDiscountsPage() {
+        if (discountsPage == null) {
+            discountsPage = new DiscountsPage(driver);
+        }
+        return discountsPage;
     }
 }
